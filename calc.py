0e238f40930afcb7bd0e98dd0736070c5d702b07
blessy raju <blessysherin96@gmail.com>
2019-02-23 12:46:55 +0530

    first commit

---
+++ calc.py
@@ -0,0 +1,10 @@
+import add 
+
+
+# num1=int(input("enter first number"))
+# num2=int(input("enter second number"))
+# result=add.addition(num1,num2)
+# print(result)
+
+print(__name__)
+add.display()
